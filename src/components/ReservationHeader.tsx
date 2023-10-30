8a5473950486c4d4c44525de0aea2ae2f9b0eeac
baomei1996 <qhal4686@gmail.com>
2023-10-31 00:59:00 +0900

    Feat: ReservationHeader component 생성 및 적용

---
+++ src/components/ReservationHeader.tsx
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+import Button from "@components/Button";
+import GoBackIconSrc from "@icons/keyboard_backspace.svg";
+import CloseIconSrc from "@icons/close.svg";
+import { useNavigate } from "react-router-dom";
+
+export default function ReservationHeader() {
+    const navigate = useNavigate();
+    const onClickGoBack = (): void => {
+        navigate(-1);
+    };
+
+    return (
+        <Container>
+            <LeftContainer>
+                <GoBackButton onClick={onClickGoBack}>
+                    <IconImg src={GoBackIconSrc} />
+                </GoBackButton>
+            </LeftContainer>
+            <CenterContainer>
+                <TitleText>New Reservation</TitleText>
+            </CenterContainer>
+            <RightContainer>
+                <CloseIcon>
+                    <IconImg src={CloseIconSrc} />
+                </CloseIcon>
+            </RightContainer>
+        </Container>
+    );
+}
+
+const Container = styled.div`
+    padding: 20px;
+    border-top-right-radius: 10px;
+    border-top-left-radius: 10px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const LeftContainer = styled.div`
+    flex: 1;
+    display: flex;
+    justify-content: flex-start;
+`;
+
+const CenterContainer = styled.div`
+    flex: 1;
+    display: flex;
+    justify-content: center;
+`;
+
+const RightContainer = styled.div`
+    flex: 1;
+    display: flex;
+    justify-content: end;
+`;
+
+const GoBackButton = styled(Button)`
+    padding: 10px;
+    display: flex;
+    align-items: center;
+`;
+
+const IconImg = styled.img``;
+
+const TitleText = styled.p`
+    margin: 0;
+    font-size: 28px;
+    line-height: 32px;
+    color: rgb(70, 65, 61);
+    white-space: nowrap;
+`;
+
+const CloseIcon = styled.div`
+    width: 45px;
+    height: 45px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: 130px;
+`;
--- src/screens/ReservationScreen.tsx
+++ src/screens/ReservationScreen.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import styled from "styled-components";
+import ReservationHeader from "@components/ReservationHeader";
 
 export default function ReservationScreen() {
-    return <div>ReservationScreen</div>;
+    return (
+        <Container>
+            <ReservationHeader />
+        </Container>
+    );
 }
+
+const Container = styled.div`
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+    background-color: rgb(255, 255, 255);
+`;
