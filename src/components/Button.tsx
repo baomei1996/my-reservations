747918df44da9f7d56019d45b04b94a9f5106358
baomei1996 <qhal4686@gmail.com>
2023-11-05 01:18:27 +0900

    Feat: Button 컴포넌트 tag 변경하고 스타일 추가함

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-interface IButtonProps extends React.HTMLAttributes<HTMLDivElement> {
+interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     children: React.ReactNode;
 }
 
@@ -9,9 +9,11 @@ export default function Button({ children, ...props }: IButtonProps) {
     return <Container {...props}>{children}</Container>;
 }
 
-const Container = styled.div`
+const Container = styled.button`
     border-radius: 10px;
     background-color: rgb(252, 252, 252);
     box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
     cursor: pointer;
+    outline: none;
+    border: none;
 `;
