c154c16a30d4065dea1fd607c5592f0243d19c75
baomei1996 <qhal4686@gmail.com>
2023-11-05 04:07:27 +0900

    Feat: EditReservation UI 추가

--- src/screens/EditReservationScreen.tsx
+++ src/screens/EditReservationScreen.tsx
@@ -1,7 +1,25 @@
+import React, { useContext } from "react";
 import styled from "styled-components";
+import ReservationHeader from "@components/ReservationHeader";
+import ReservationForm from "@components/ReservationForm";
+import { useParams } from "react-router-dom";
+import { ReservationContext } from "@contexts/ReservationContext";
 
 export default function EditReservationScreen() {
-    return <Container>edit</Container>;
+    const { id } = useParams();
+    const { getReservation } = useContext(ReservationContext);
+    const reservation = getReservation(id ?? "");
+
+    if (!reservation) {
+        return <h3>Oops something went wrong.</h3>;
+    }
+
+    return (
+        <Container>
+            <ReservationHeader isEditMode />
+            <ReservationForm isEditMode reservation={reservation} />
+        </Container>
+    );
 }
 
 const Container = styled.div`
